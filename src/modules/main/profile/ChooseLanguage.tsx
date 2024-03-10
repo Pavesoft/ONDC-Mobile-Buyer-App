99fccb3180646e149f29f5eefead0ebb453f45fc
Neeraj Madake <neerajmadake18@gmail.com>
2024-03-10 18:18:02 +0530

    Add logic to remove pick address page after visiting choose language page

--- src/modules/main/profile/ChooseLanguage.tsx
+++ src/modules/main/profile/ChooseLanguage.tsx
@@ -1,22 +1,38 @@
+import React from 'react';
 import {StyleSheet, View, FlatList, TouchableOpacity} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
-import React from 'react';
+import {useSelector} from 'react-redux';
 import {Text} from 'react-native-paper';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {useTranslation} from 'react-i18next';
 import {useAppTheme} from '../../../utils/theme';
 import i18n from 'i18next';
+import {setStoredData} from '../../../utils/storage';
 
 const ChooseLanguage = () => {
   const theme = useAppTheme();
+  const {address} = useSelector(({addressReducer}) => addressReducer);
   const navigation = useNavigation();
   const {t} = useTranslation();
   const styles = makeStyles(theme.colors);
 
-  const handleChangeLanguage = language => {
-    i18n.changeLanguage(language, () => {
-      navigation.goBack();
-    });
+  const handleChangeLanguage = async (language: string) => {
+    i18n.changeLanguage(language);
+    await setStoredData('appLanguage', language);
+
+    // Check if address preselected, if so directly navigate
+    // to dashboard
+    if (address && address.address) {
+      navigation.reset({
+        index: 0,
+        routes: [{name: 'Dashboard'}],
+      });
+    } else {
+      navigation.reset({
+        index: 0,
+        routes: [{name: 'AddressList', params: {navigateToDashboard: true}}],
+      });
+    }
   };
 
   const menu = [
@@ -32,12 +48,18 @@ const ChooseLanguage = () => {
         handleChangeLanguage('en');
       },
     },
+    {
+      title: t('Choose Language.Marathi'),
+      onPress: () => {
+        handleChangeLanguage('ma');
+      },
+    },
   ];
 
   return (
     <View style={styles.container}>
       <Text variant={'bodyMedium'} style={styles.inputLabel}>
-        Select a language
+        {t('Choose Language.Select a language')}
       </Text>
       <FlatList
         contentContainerStyle={styles.listContainer}
